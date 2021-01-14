cbcc3e96d9a94443807c83cc8542dae8362eec7c
Xiao-R-Y <59918171+Xiao-R-Y@users.noreply.github.com>
2021-01-14 21:26:15 +0800

    完善增删改函数，删除大量测试注释，增加函数功能说明注释

--- dml.c
+++ dml.c
@@ -2,11 +2,10 @@
 #include<string.h>
 #include<stdlib.h>
 #include<math.h>
-//#include<sstream>
 
 extern int OpenTable(char *name, PTableMode FieldSet);
 
-//分割字符串函数，用来实现对指定字符的分割
+/**分割字符串函数，用来实现对指定字符的分割,指针类型*/
 void split(char *src,const char *separator,char **dest,int *numstr) {
 	/*
 		src 源字符串的首地址(buf的地址) 
@@ -29,6 +28,7 @@ void split(char *src,const char *separator,char **dest,int *numstr) {
     *numstr = count;
 } 	
 
+/**分割字符串函数，数组类型*/
 void split1(char *src,const char *separator,char dest[50][15],int *numstr) {
 	/*
 		src 源字符串的首地址(buf的地址) 
@@ -56,8 +56,7 @@ void split1(char *src,const char *separator,char dest[50][15],int *numstr) {
     *numstr = count;
 } 	
 
-
-
+/**插入函数，实现对数据按照表结构的插入功能*/
 void Insert(char* name,char* value){
     //name字段包含了表名（字段名）...
     //value字段包含了values（字段值）...
@@ -79,87 +78,27 @@ void Insert(char* name,char* value){
     char values[50][15]={};//用来存放分割之后的字段值
     //将name字段的值赋值给str
     strcpy(str,name);
-    //for(int i = 0;i<strlen(name);i++){
-        //str[i] = name[i];
-    //}
-    /* for(int i=strlen(name);i<100;i++){
-        str[i] = '\0';
-    } */
-    //printf("%s\n",str);//检查命令是否赋值合格
     //第一轮分割，分割出表名
     split(str,"(",tempname,&numstr);
     for(int i = 0; i<strlen(tempname[0]);i++){
         tname[i] = tempname[0][i]; 
     }
     strcpy(str,tempname[1]);
-    /* for(int i = 0; i<strlen(tempname[1]); i++){
-        str[i] = tempname[1][i]; 
-    } */
-    //str[strlen(tempname[1])] = '\0';
-    //printf("%s\n",str);//用来检查待实现的命令分割是否合格
     //第二轮分割，分割出纯粹的field值
     split(str,")",tempname,&numstr);
     strcpy(str,tempname[0]);
-    /* for(int i = 0; i<strlen(tempname[0]);i++){
-        str[i] = tempname[0][i];
-    }
-    str[strlen(tempname[0])] = '\0'; */
-    //printf("%s\n",str);//用来检查待实现的命令分割是否合格
     //第三轮分割，分割出各个字段的值，保证写入
     split1(str,",",field,&numstr);
-    //用来检查最终分割字段名是否合格
-    /*
-    for(int i = 0 ;i<numstr;i++){
-        printf("%s\n",field[i]);
-    }
-    */
     //将values字段的值赋值给str
-    /* for(int i = 0;i<strlen(value);i++){
-        str[i] = value[i];
-    } */
     strcpy(str,value);
-    /* for(int i=strlen(name);i<100;i++){
-        str[i] = '\0';
-    } */
-    //printf("%s\n",str);//检查命令是否赋值合格
     //第一轮分割，分割出表values关键词
     split(str,"(",tempname,&numstr);
     strcpy(str,tempname[1]);
-    /* for(int i = 0; i<strlen(tempname[1]); i++){
-        str[i] = tempname[1][i]; 
-    } */
-    /* for(int i = strlen(tempname[1]);i<100;i++){
-        str[i] = '\0';
-    } */
-    //printf("%s\n",str);//用来检查待实现的命令分割是否合格
     //第二轮分割，分割出纯粹的value值
     split(str,")",tempname,&numstr);
-    /* for(int i = 0; i<strlen(tempname[0]);i++){
-        str[i] = tempname[0][i];
-    }
-    str[strlen(tempname[0])] = '\0'; */
     strcpy(str,tempname[0]);
-    //printf("%s\n",str);//用来检查待实现的命令分割是否合格
     //第三轮分割，分割出各个字段的值，保证写入
     split1(str,",",values,&numstr);
-    /* for(int i = 0 ;i<numstr;i++){
-        printf("%s\n",values[i]);
-    } */
-
-    /* for(int i = 0;i<numstr;i++){
-        values[i][strlen(values[i])] = '\40';
-        for(int j = strlen(values[i])+1;j<FIELD_NAME_LENGTH;j++){
-            values[i][j]='\40';
-        }
-    } */
-    //用来检查最终分割字段名是否合格
-    /*
-    for(int i = 0 ;i<numstr;i++){
-        printf("%s\n",values[i]);
-        //printf("%p\n",values[i]);//指示指针地址，发现指针地址之差为2，说明存在问题
-    }
-    */
-    
    /*获得表结构列表值*/
     TableMode FieldSet[MAX_SIZE];
     int num = OpenTable(tname, FieldSet);//局部变量，用来得到表的字段个数值
@@ -193,7 +132,7 @@ void Insert(char* name,char* value){
         strcat(dat,".dat");
         //printf("%s\n",dat);//检查表名是否连接合格
         dp = fopen(dat, "ab+");
-
+        //写入格式化，为了方便查询
         fwrite("#",sizeof(char), 1, dp);
         for( int i = 0; i<num ;i++){
             if(strlen(forinsertvalue[i])==0){
@@ -202,12 +141,10 @@ void Insert(char* name,char* value){
             else if(strlen(forinsertvalue[i])!=0){
                 if (strcmp(FieldSet[i].sType,"int")==0){
                     int x = atoi(forinsertvalue[i]);
-                    //printf("%d",x);
                     fwrite(&x,sizeof(int),1,dp);
                 }
                 else if(strcmp(FieldSet[i].sType,"double")==0){
                     double x =atof(forinsertvalue[i]);
-                    printf("%lf",x);
                     fwrite(&x,sizeof(double),1,dp);
                 }
                 else{
@@ -215,14 +152,16 @@ void Insert(char* name,char* value){
                 }
                 
             }
-            //printf("%p\n",dp);
+
         }
         fclose(dp);  
     }
         
     }
-    //printf("insert into %s %s\n",name,value);
+    printf("Insert into %s %s successfully!\n",name,value);
 }
+
+/**删除函数，来对指定数据进行删除*/
 void Delete(char* tableName,char* colName,char* value){
     /**
      * 为了实现删除操作
@@ -236,6 +175,7 @@ void Delete(char* tableName,char* colName,char* value){
      * 全部读取完毕关闭原文件并删除
      * 将临时文件名改为原文件名
     */
+   /**判断数据库和表是否打开*/
    if(dopens == 0){
         printf("数据库未打开，无法执行删除操作!\n");
     }
@@ -250,23 +190,16 @@ void Delete(char* tableName,char* colName,char* value){
     char tvalue[FIELD_NAME_LENGTH]={0};//用来暂时存放读取的数据
    
     char tflag;//用来存放读到的标志位
-    /* for(int i = 0 ;i<strlen(tableName);i++){
-        tname[i] = tableName[i];
-    } */
+    //获得表名，字段数目
     strcpy(tname,tableName);
-    
-    //printf("%s",tname);
-    
-
     TableMode FieldSet[MAX_SIZE];
-
     int num = OpenTable(tname,FieldSet);
     strcat(tname,".dat");
     //打开对应的文件
     dp = fopen(tname,"rb+");
     dpt = fopen(tname,"rb+");
-    int databyte = 0;//不计算了开始的标志位
-    int desbyte = 0;//不计算了开始的标志位
+    int databyte = 0;//不计算开始的标志位
+    int desbyte = 0;//不计算开始的标志位
     //按照数据类型获得总共的字节数
     for(int i =0; i<num ; i++){
         if(strcmp(FieldSet[i].sType,"int")==0){
@@ -278,10 +211,7 @@ void Delete(char* tableName,char* colName,char* value){
         else{
             databyte = databyte + FieldSet[i].iSize;
         }  
-        //printf("%d\n",databyte);  
     }
-    //databyte = databyte +1;
-    
     //用来标志等待修改的列的位置
     int flag = 0;
     for(flag = 0;flag < num;flag++){
@@ -300,36 +230,24 @@ void Delete(char* tableName,char* colName,char* value){
             desbyte = desbyte + FieldSet[i].iSize;
         }    
     }
-    
-    //desbyte = desbyte + 1;
-    //printf("%d\n",databyte);
-    //printf("%d\n",desbyte);
     int it = 0;
-    //printf("%p\n",dpt);
-    //printf("%s\n",value);
     //读取当前文件中的内容，并且处理文件结果
     char ch ;
     while(!feof(dp)){
         int ml;
         ml = fread(&ch,sizeof(char),1,dp);
-        //printf("%c",ch);
-        if(!ml) break;
+        if(!ml) break;//判断读取的值是否是格式化的有效
 
-        //printf("%d\n",it);
-        
         tp = fopen("temp.dat","ab+");
         fseek(dpt,long(desbyte+1),SEEK_CUR);//相对于当前读到待检测数据；
-        //printf("%d\n",xxx);
-        //printf("%p\n",dp);
-        //printf("%p\n",dpt);
+
         int seekflag = 0;//0代表要写入temp 文件，1不写
         int y = 0;//指示代检测的数据读取之后字节数
-
+        //判断待删除的字段的数据类型
         if(strcmp(FieldSet[flag].sType,"int")==0){
             int x;
             y = sizeof(int);
             fread(&x,sizeof(int),1,dpt);
-            //printf("zhe int shuchu\n");
             if(x==atoi(value)){
                 seekflag = 1;
             }
@@ -338,27 +256,24 @@ void Delete(char* tableName,char* colName,char* value){
             double x;
             y = sizeof(double);
             fread(&x,sizeof(double),1,dpt);
-            //printf("%f\n",x);
-            //printf("zhe double shuchu\n");
             if(fabs(x-atof(value))<1e-6){
                 seekflag = 1;
             }
         }
         else{
             char x[50]={};
-            //printf("%s\n",x);
             y = FieldSet[flag].iSize;
             fread(x,sizeof(char),FieldSet[flag].iSize,dpt);
-            //printf("zhe char shuchu\n");
-            //printf("%s\n",x);
             if(strcmp(x,value)==0){
                 seekflag = 1;
             }
         }
+        //判断是否是all，如果是all直接全部删除
+        if(strcmp(value,"all")==0){
+            seekflag = 1;
+        }
         if(seekflag == 0){
-            
             char dataforinsert[2000];
-            //printf("%s",dataforinsert);
             fwrite(&ch,sizeof(char),1,tp);
             fread(dataforinsert,sizeof(char),databyte,dp);
             fwrite(dataforinsert,sizeof(char),databyte,tp);
@@ -366,13 +281,7 @@ void Delete(char* tableName,char* colName,char* value){
         else{
             fseek(dp,long(databyte),SEEK_CUR);
         }
-        //fseek(dp,databyte,SEEK_CUR);
         fseek(dpt,long(databyte-desbyte-y),SEEK_CUR);
-        /* printf("%d\n",databyte);
-        printf("%d\n",desbyte);
-        printf("%d\n",y);
-        printf("%d\n",databyte-desbyte-y);
-        printf("\n"); */
         it++;
             
         fclose(tp);
@@ -382,9 +291,10 @@ void Delete(char* tableName,char* colName,char* value){
     remove(tname);
     rename("temp.dat",tname);
     }
-    //printf("delete from %s where %s = %s\n",tableName,colName,value);
+    printf("delete from %s where %s = %s successfully!\n",tableName,colName,value);
 }
-void update(char* tableName,char* newColName,char* newValue,char* colName,char* value){
+/**更新函数，对指定数据进行更新操作*/
+void update(char* newColName ,char* newValue,char* tableName,char* colName,char* value){
     /**
      * 为了实现修改操作
      * 需要建立一个临时文件
@@ -396,23 +306,195 @@ void update(char* tableName,char* newColName,char* newValue,char* colName,char* 
      * 原文件读完之后关闭原文件并删除
      * 将临时文件名称该为原文件名称
     */
+    /**判断是否打开数据库和表文件*/
+    if(dopens == 0){
+        printf("数据库未打开，无法执行删除操作!\n");
+    }
+    else if(topens == 0){
+        printf("表未打开，无法执行删除操作！\n");
+    }
+    else//检测有效，可以开始获取指令并录入信息
+    {
     FILE *tp;//用来创建指向的临时文件
+    FILE *dpt;//指向待检测数据；
     char tname[MAX_SIZE]={0};//用来存放表名
+    int databyte = 0;//获得一条数据的总字节数
+    int desbyte = 0;//获得待检测处数据前的总字节数
+    int changebyte = 0;//获得待修改数据前的总字节数
     //复制表名，添加表名后缀
-    for(int i = 0 ;i<strlen(tableName);i++){
-        tname[i] = tableName[i];
+    strcpy(tname,tableName);
+    TableMode FieldSet[MAX_SIZE];
+    int num = OpenTable(tname,FieldSet);
+    //得到一条数据的总字节数
+    for(int i =0; i<num ; i++){
+        if(strcmp(FieldSet[i].sType,"int")==0){
+            databyte = databyte + sizeof(int);
+        }
+        else if(strcmp(FieldSet[i].sType,"double")==0){
+            databyte =databyte + sizeof(double);
+        }
+        else{
+            databyte = databyte + FieldSet[i].iSize;
+        }   
+    }
+    //找到待检测的数据的位置
+    int flag = 0;
+    for(flag = 0;flag < num;flag++){
+        if(strcmp(colName,FieldSet[flag].sFieldName)==0)//字符串相等
+        break;
+    }
+    //获取待检测数据所在位置的字节数
+    for(int i =0; i<flag ; i++){
+        if(strcmp(FieldSet[i].sType,"int")==0){
+            desbyte = desbyte + sizeof(int);
+        }
+        else if(strcmp(FieldSet[i].sType,"double")==0){
+            desbyte =desbyte + sizeof(double);
+        }
+        else{
+            desbyte = desbyte + FieldSet[i].iSize;
+        }    
+    }
+    //找到待修改的数据
+    int change = 0;
+    for(change= 0;change < num;change++){
+        if(strcmp(newColName,FieldSet[change].sFieldName)==0)//字符串相等
+        break;
+    }
+    //获得待修改数据所在位置的字节数
+    for(int i =0; i<change ; i++){
+        if(strcmp(FieldSet[i].sType,"int")==0){
+            changebyte = changebyte + sizeof(int);
+        }
+        else if(strcmp(FieldSet[i].sType,"double")==0){
+            changebyte =changebyte + sizeof(double);
+        }
+        else{
+            changebyte = changebyte + FieldSet[i].iSize;
+        }    
     }
     strcat(tname,".dat");
     //打开文件并准备开始判断和修改操作
     dp = fopen(tname,"rb+");
+    dpt = fopen(tname,"rb+");
+    int it = 0;
+    char ch ;
     while(!feof(dp)){
+        int ml;
+        ml = fread(&ch,sizeof(char),1,dp);
+        if(!ml) break;
         tp = fopen("temp.dat","ab+");
 
+        fseek(dpt,long(desbyte+1),SEEK_CUR);//找到待检测的数据的位置；
+        int seekflag = 0;//0代表不修改写入temp 文件，1修改后写入
+        int y = 0;//指示代检测的数据读取之后字节数
+        //判断指示数据的类型，并根据字段类型读写
+        if(strcmp(FieldSet[flag].sType,"int")==0){
+            int x;
+            y = sizeof(int);
+            fread(&x,sizeof(int),1,dpt);
+            if(x==atoi(value)){
+                seekflag = 1;
+            }
+        }
+        else if(strcmp(FieldSet[flag].sType,"double")==0){
+            double x;
+            y = sizeof(double);
+            fread(&x,sizeof(double),1,dpt);;
+            if(fabs(x-atof(value))<1e-6){
+                seekflag = 1;
+            }
+        }
+        else{
+            char x[50]={};
+            y = FieldSet[flag].iSize;
+            fread(x,sizeof(char),FieldSet[flag].iSize,dpt);
+            if(strcmp(x,value)==0){
+                seekflag = 1;
+            }
+        }
+        //如果字段指示为all,则对应位置的数据全部做修改
+        if(strcmp(value,"all")==0){
+            seekflag = 1;
+        }
+        if(seekflag == 0){
+            
+            char dataforinsert[2000];
+            fwrite(&ch,sizeof(char),1,tp);
+            fread(dataforinsert,sizeof(char),databyte,dp);
+            fwrite(dataforinsert,sizeof(char),databyte,tp);
+        }
+        else{
+            char dataforinsert[2000];
+            fread(dataforinsert,sizeof(char),databyte,dp);//读取到待修改的数据，以二进制方式
+            /**
+             * 如果准备修改的是int类型的数据，
+             * 现在得到的是字符串1
+             * 读到的是int 1转码的二进制
+             * 需要修改的是转成int之后再用二进制写入用char解释的码
+             * 所以第一步是char转成int使用atoi函数
+             * 第二步是int转成二进制，放到char数组
+             * 第三步是在对应的位置转换char数组的内容
+             * 
+            */
+           //判断待修改位置的数据类型，决定修改的方案
+           if(strcmp(FieldSet[change].sType,"int")==0){
+               int x;
+               char yt[sizeof(int)];
+               x = atoi(newValue);
+               FILE *tmp;
+               tmp = fopen("changedata","ab+");
+               fwrite(&x,sizeof(int),1,tmp);
+               fread(yt,sizeof(char),sizeof(int),tmp);
+               fclose(tmp);
+               remove("changedata");
+               for(int i = changebyte;i<changebyte+sizeof(int);i++){
+                   dataforinsert[i] = yt[i-changebyte];
+               }
+               fwrite(&ch,sizeof(char),1,tp);
+               fwrite(dataforinsert,sizeof(char),databyte,tp);
+           }
+           else if(strcmp(FieldSet[flag].sType,"double")==0){
+               double x;
+               char yt[sizeof(double)];
+               x = atof(newValue);
+               FILE *tmp;
+               tmp = fopen("changedata","ab+");
+               fwrite(&x,sizeof(double),1,tmp);
+               fread(yt,sizeof(char),sizeof(double),tmp);
+               fclose(tmp);
+               remove("changedata");
+               for(int i = changebyte;i<changebyte+sizeof(double);i++){
+                   dataforinsert[i] = yt[i-changebyte];
+               }
+               fwrite(&ch,sizeof(char),1,tp);
+               fwrite(dataforinsert,sizeof(char),databyte,tp);
+           }
+           else{
+               char yt[FILE_NAME_LENGTH] = {};
+               strcpy(yt,newValue);
+               for(int i = changebyte;i<changebyte+FieldSet[change].iSize;i++){//这里找一下怎么弄休止符号
+                   if(i<changebyte+strlen(yt)){
+                        dataforinsert[i] = yt[i-changebyte];
+                   }
+                   else{
+                       dataforinsert[i] = '\0';
+                   }
+               }
+               fwrite(&ch,sizeof(char),1,tp);
+               fwrite(dataforinsert,sizeof(char),databyte,tp);
+           }
+
+        }
+        fseek(dpt,long(databyte-desbyte-y),SEEK_CUR);
+        it++; 
 
         fclose(tp);
     }
+    fclose(dpt);
     fclose(dp);
     remove(tname);
     rename("temp.dat",tname);
-    //printf("update %s = %s from %s where %s = %s\n",tableName,newColName,newValue,colName,value);
+    }
+    printf("update %s = %s from %s where %s = %s successfully!\n",tableName,newColName,newValue,colName,value);
 }
